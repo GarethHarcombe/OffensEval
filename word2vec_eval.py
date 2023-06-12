027ac00ee101a4c4f1d163f2d4c6bb45ab9f5b34
Gareth Harcombe <gharcombe2001@outlook.com>
2023-06-12 15:14:40 +1200

    sgns eval and train update

--- word2vec_eval.py
+++ word2vec_eval.py
@@ -11,6 +11,8 @@ from gensim.models import KeyedVectors
 import nltk
 import string
 import re
+import gensim.downloader as api
+from statistics import stdev
 
 
 DATA_DIR = "/csse/users/grh102/Documents/cosc442/OffensEval/OLID/"
@@ -30,17 +32,32 @@ def tokenise(d):
     return nltk.word_tokenize(emoji_pattern.sub(r'', text_p))
 
 
+def load_sgns_embedding(filepath):
+    embedding = dict()
+    vocab = set()
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+        vocab_size, embedding_size = lines[0].split()
+        for i in range(1, len(lines)):
+            line = lines[i].split()
+            vocab.add(line[0])
+            embedding[line[0]] = np.array([float(num) for num in line[1:]])
+
+    return embedding, vocab
+
+
 class Dataset(torch.utils.data.Dataset):
 
     def __init__(self, df):
         self.labels = [LABELS[label] for label in df['subtask_a']]
         self.texts = [tokenise(text) for text in df['tweet']]
-        self.word2vec = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+        # self.word2vec = load_sgns_embedding("vectors.txt")
+        # self.word2vec = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+        self.word2vec = api.load("word2vec-google-news-300")
         self.word2vec.add_vector("[PAD]", np.zeros(self.word2vec.vector_size))
 
         self.texts = [[word for word in text if word in self.word2vec.index_to_key] for text in self.texts]
         self.masks = [0] * len(self.texts)
-        print(self.word2vec.index_to_key)
         for i, text in enumerate(self.texts):
             # print(text)
             if len(text) > 64:
@@ -48,10 +65,8 @@ class Dataset(torch.utils.data.Dataset):
             elif len(text) < 64:
                 self.texts[i] = text + ["[PAD]"] * (64 - len(text))
 
-            self.masks[i] = torch.tensor([1] * len(self.texts[i]) + [0] * (64 - len(self.texts[i])))
+            self.masks[i] = torch.unsqueeze(torch.tensor([1] * min(len(text), 64) + [0] * max((64 - len(text)), 0)), 0)
             self.texts[i] = torch.tensor([self.word2vec[word] for word in self.texts[i]])
-
-        # print(self.texts)
 
     def classes(self):
         return self.labels
@@ -91,27 +106,24 @@ class Word2VecClassifier(nn.Module):
 
         if self.classification == "regression":
             self.dropout = nn.Dropout(dropout)
-            self.linear = nn.Linear(768, len(LABELS))
+            self.linear = nn.Linear(300, len(LABELS))
             self.relu = nn.ReLU()
 
         if self.classification == "lstm":
-            self.lstm = nn.LSTM(768, 256, batch_first=True, bidirectional=True)
-            self.linear = nn.Linear(256*2, len(LABELS))
+            self.lstm = nn.LSTM(300, 128, batch_first=True, bidirectional=True)
+            self.linear = nn.Linear(128*2, len(LABELS))
 
-    def forward(self, text):
-        print(text)
-        input, mask = text
+    def forward(self, input, mask):
         if self.classification == "lstm":
             # https://stackoverflow.com/questions/65205582/how-can-i-add-a-bi-lstm-layer-on-top-of-bert-model
-            # sequence_output has the following shape: (batch_size, sequence_length, 768)
+            # sequence_output has the following shape: (batch_size, sequence_length, 300)
             lstm_output, (h,c) = self.lstm(input) ## extract the 1st token's embeddings
-            hidden = torch.cat((lstm_output[:,-1, :256],lstm_output[:,0, 256:]),dim=-1)
-            linear_output = self.linear(hidden.view(-1,256*2)) ### assuming that you are only using the output of the last LSTM cell to perform classification
+            hidden = torch.cat((lstm_output[:,-1, :128],lstm_output[:,0, 128:]),dim=-1)
+            linear_output = self.linear(hidden.view(-1,128*2)) ### assuming that you are only using the output of the last LSTM cell to perform classification
             return linear_output
         
         else:
-            last_hidden_masked = text * torch.reshape(mask, (input.shape[0], input.shape[1], 1))
-        
+            last_hidden_masked = input * torch.reshape(mask, (input.shape[0], input.shape[1], 1))
             # pooled embeddings
             if self.pooling_method == "sum":
                 bert_output = torch.sum(last_hidden_masked, dim=1)
@@ -128,14 +140,79 @@ class Word2VecClassifier(nn.Module):
             return final_layer
 
 
+def train(model, train_data, val_data, learning_rate, epochs):
+
+    train, val = train_data, val_data
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr= learning_rate)
+
+    if use_cuda:
+        model = model.cuda()
+        criterion = criterion.cuda()
+
+    for epoch_num in range(epochs):
+        total_acc_train = 0
+        total_loss_train = 0
+
+        for train_input, train_label in tqdm(train_dataloader):
+            
+            train_label = train_label.to(device)
+            input = train_input[0].to(device)
+            mask = train_input[1].to(device)
+
+            output = model(input, mask)
+            
+            batch_loss = criterion(output, train_label.long())
+            total_loss_train += batch_loss.item()
+            
+            acc = (output.argmax(dim=1) == train_label).sum().item()
+            total_acc_train += acc
+
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+        
+        total_acc_val = 0
+        total_loss_val = 0
+
+        with torch.no_grad():
+
+            for val_input, val_label in val_dataloader:
+
+                val_label = val_label.to(device)
+                input = val_input[0].to(device)
+                mask = val_input[1].to(device)
+
+                output = model(input, mask)
+
+                batch_loss = criterion(output, val_label.long())
+                total_loss_val += batch_loss.item()
+                
+                acc = (output.argmax(dim=1) == val_label).sum().item()
+                total_acc_val += acc
+        
+        print(
+            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+            | Train Accuracy: {total_acc_train / len(train_data): .3f} \
+            | Val Loss: {total_loss_val / len(val_data): .3f} \
+            | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+
+
 def evaluate(model, test_data):
-    test = Dataset(test_data)
+    test = test_data
 
     test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
 
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-
+    
     if use_cuda:
         model = model.cuda()
 
@@ -147,8 +224,10 @@ def evaluate(model, test_data):
         for test_input, test_label in test_dataloader:
 
             test_label = test_label.to(device)
-            
-            output = model(test_input)
+            input = test_input[0].to(device)
+            mask = test_input[1].to(device)
+
+            output = model(input, mask)
  
             test_labels.append(test_label)
             preds.append(output.argmax(dim=1))
@@ -161,27 +240,47 @@ def evaluate(model, test_data):
     test_labels = [label.cpu().detach().numpy() for label in test_labels]
     test_labels = list(itertools.chain.from_iterable(test_labels))
     
-    print_results(test_labels, preds)
-    
     print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
+
+    return print_results(test_labels, preds)
+
+
+def test_many(df_train, df_val, df_test, classification, pooling_method):
+    EPOCHS = 2
+    LR = 1e-6
+    
+    print("training word2vec with {} classification method, and {} pooling".format(classification, pooling_method))
+
+    results = []
+    for i in range(5):
+        model = Word2VecClassifier(classification=classification, pooling_method=pooling_method)
+        train(model, df_train, df_val, LR, EPOCHS)
+        results.append(evaluate(model, df_test))
+
+    print(f"Results: {results} \n Mean: {sum(results)/len(results)} \n Stdev: {stdev(results)}")
+    with open("results.txt", 'a') as f:
+        f.write("training word2vec with {} classification method, and {} pooling\n".format(classification, pooling_method))
+        f.write(f"Results: {results} \nMean: {sum(results)/len(results)} \nStdev: {stdev(results)}")
 
 
 def main():
     df_train, df_val, df_test = get_dfs()
+    df_train, df_val, df_test = Dataset(df_train), Dataset(df_val), Dataset(df_test)
 
     classification = "regression"
     for pooling_method in ["sum", "average", "max"]:
-        print("training BERT with {} classification method, and {} pooling".format(classification, pooling_method))
-        model = Word2VecClassifier(classification=classification, pooling_method=pooling_method)
-        evaluate(model, df_test)
-        torch.save(model.state_dict(), MODEL_DIR + "bert_" + classification + "_" + pooling_method + ".pth")
+        # path = MODEL_DIR + "word2vec_" + classification + "_" + pooling_method + ".pth"
+
+        test_many(df_train, df_test, df_val, classification, pooling_method)
+        # torch.save(model.state_dict(), path)
 
     classification = "lstm"
     for pooling_method in ["all"]:
-        print("training BERT with {} classification method, and {} pooling".format(classification, pooling_method))
-        model = Word2VecClassifier(classification=classification, pooling_method=pooling_method)
-        evaluate(model, df_test)
-        torch.save(model.state_dict(), MODEL_DIR + "bert_" + classification + ".pth")
+        
+        # path = MODEL_DIR + "word2vec_" + classification + ".pth"
+        
+        test_many(df_train, df_test, df_val, classification, pooling_method)
+        # torch.save(model.state_dict(), path)
 
 
 if __name__ == "__main__":
--- word2vec_train.py
+++ word2vec_train.py
@@ -1,17 +1,54 @@
 from gensim.models.word2vec import Word2Vec
 import gensim.downloader as api
-
-print("loading file...")
-corpus = api.load('wiki-english-20171001')  # download the corpus and return it opened as an iterable
-print("file loaded")
-model = Word2Vec(corpus, vector_size=768, workers=20, sg=1)
-print("model trained???")
-model.save("word2vec.model")
+import string
+import nltk
+from smart_open import smart_open
+import sentencepiece as spm
 
 
-# Store just the words + their trained embeddings.
-word_vectors = model.wv
-word_vectors.save("word2vec.wordvectors")
+class WikiCorpus:
 
-# Load back with memory-mapping = read-only, shared across processes.
-# wv = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+    def __init__(self, corpus) -> None:
+        self.corpus = corpus
+
+    def __len__(self):
+        return len(self.corpus)
+
+    def __iter__(self):
+        for article in self.corpus:
+            for section_text in article["section_texts"]:
+                text_p = "".join([char for char in section_text.lower() if char not in (string.punctuation + "—–”") and not char.isnumeric()])
+                yield " ".join(nltk.word_tokenize(text_p))
+
+def write_data_to_txt(corpus):
+    wiki_corpus = iter(WikiCorpus(corpus))
+    with smart_open('wiki-english-20171001.txt', 'w', encoding="utf-8") as outfile:
+        iterating = True
+        while iterating:
+            try:
+                outfile.write(next(wiki_corpus))
+            except StopIteration:
+                iterating = False
+
+
+def train_embedding(corpus):
+    model = Word2Vec(WikiCorpus(corpus), vector_size=768, workers=20, sg=1, epochs=1)
+    print("model trained")
+    print(len(model.wv.index_to_key))
+
+    # Store just the words + their trained embeddings.
+    word_vectors = model.wv
+    word_vectors.save("word2vec.wordvectors")
+
+
+
+if __name__ == "__main__":
+    # print("loading file...")
+    # corpus = api.load('wiki-english-20171001')  # download the corpus and return it opened as an iterable
+    # print("file loaded")
+    
+    # write_data_to_txt(corpus)
+
+    spm.SentencePieceTrainer.train(input='wiki-english-20171001.txt', model_prefix='m', vocab_size=100_000)
+
+    # train_embedding(corpus)
